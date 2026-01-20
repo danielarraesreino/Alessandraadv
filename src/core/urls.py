5ce50a660d0afeafd34c44cac1d9703ab34d829b
Daniel <coletivoaruatemvoz@gmail.com>
2026-01-20 12:00:06 -0300

    debug: Minimal configuration to isolate 500 error

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,14 +6,14 @@ from ninja import NinjaAPI
 from ninja.errors import ConfigError
 from django.views.generic import TemplateView
 
-from apps.portals import views as portals_views
+# from apps.portals import views as portals_views
 
 from core.api import api
 
 
 from core import views
 from core.views import role_based_redirect
-from apps.whatsapp.api import api as whatsapp_api
+# from apps.whatsapp.api import api as whatsapp_api
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
@@ -21,14 +21,14 @@ urlpatterns = [
     path("role-redirect/", role_based_redirect, name="role_redirect"),
     path("admin/", admin.site.urls),
     path("api/", api.urls), # Main API
-    path("api/whatsapp/", whatsapp_api.urls), # WhatsApp API Instance
+#    path("api/whatsapp/", whatsapp_api.urls), # WhatsApp API Instance
     path("in-brief/", include("in_brief.urls")),
-    path("portal-admin/", include("admin_portal.urls")),
-    path("portal-admin/observatory/", include("apps.observatory.urls")),
-    path("acesso/", include([
-        path("", portals_views.client_login, name="client_login"),
-    ])),
-    path("portal/", include("apps.portals.urls")),
+#    path("portal-admin/", include("admin_portal.urls")),
+#    path("portal-admin/observatory/", include("apps.observatory.urls")),
+#    path("acesso/", include([
+#        path("", portals_views.client_login, name="client_login"),
+#    ])),
+#    path("portal/", include("apps.portals.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path('accounts/', include('allauth.urls')), # [NEW] Allauth URLs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
