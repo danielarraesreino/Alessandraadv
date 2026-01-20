8bb4eae19f4e3853dd65ed92215acac7af1f0955
Daniel <coletivoaruatemvoz@gmail.com>
2026-01-20 09:35:06 -0300

    fix(api): Prevent duplicate router attachment during module reloads

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,24 +3,34 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from ninja import NinjaAPI
+from ninja.errors import ConfigError
 from django.views.generic import TemplateView
-from in_brief.api.endpoints import router as in_brief_router
-from apps.intake.api.router import router as intake_router
-from apps.integrations.api.router import router as integrations_router
-from apps.portals.api.router import router as portals_router
-from apps.whatsapp.api import api as whatsapp_api
 
 from apps.portals import views as portals_views
 
 # Define Main API
 api = NinjaAPI(title="Alessandra M. Donadon API", version="1.0.0")
-api.add_router("/in-brief", in_brief_router)
-api.add_router("/intake", intake_router)
-api.add_router("/integrations", integrations_router)
-api.add_router("/portals", portals_router)
+
+# Import routers
+from in_brief.api.endpoints import router as in_brief_router
+from apps.intake.api.router import router as intake_router
+from apps.integrations.api.router import router as integrations_router
+from apps.portals.api.router import router as portals_router
+
+# Attach routers with error handling for module reloads
+try:
+    api.add_router("/in-brief", in_brief_router)
+    api.add_router("/intake", intake_router)
+    api.add_router("/integrations", integrations_router)
+    api.add_router("/portals", portals_router)
+except ConfigError:
+    # Router already attached (happens during module reload in DEBUG mode)
+    pass
+
 
 from core import views
 from core.views import role_based_redirect
+from apps.whatsapp.api import api as whatsapp_api
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
