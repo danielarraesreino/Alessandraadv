1cf8efe93b964a4f0bb2d66a6ad6f72fc6c8775d
Daniel <coletivoaruatemvoz@gmail.com>
2026-01-26 20:39:07 -0300

    style(in-brief): set app verbose name

--- src/in_brief/apps.py
+++ src/in_brief/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class InBriefConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "in_brief"
+    verbose_name = "In Brief (Conteúdos)"
